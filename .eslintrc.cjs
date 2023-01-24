c5fda4e4f5289aaccb561991c513f1e793e30aad
William Pittman <wspittman@gmail.com>
2023-01-24 15:50:14 -0800

    ESLint + Prettier together

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,7 @@ module.exports = {
 		'plugin:react/recommended',
 		'plugin:react/jsx-runtime',
 		'plugin:react-hooks/recommended',
+		'plugin:prettier/recommended',
 	],
 	rules: {
 		'@typescript-eslint/no-unused-vars': ['error', { 'ignoreRestSiblings': true }],
